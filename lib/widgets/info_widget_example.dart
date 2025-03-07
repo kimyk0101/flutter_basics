9e7be6b94913e46f73d013eda4af6f5475202e28
kimyk0101 <kimyk7131@gmail.com>
2025-03-07 15:59:54 +0900

    위젯 애니메이션

--- lib/widgets/info_widget_example.dart
+++ lib/widgets/info_widget_example.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class InfoWidgetExample extends StatelessWidget {
@@ -5,11 +7,48 @@ class InfoWidgetExample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "정보 표시 위젯 예시",
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-      ),
+    // return Center(
+    //   child: Text(
+    //     "정보 표시 위젯 예시",
+    //     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    //   ),
+    // );
+    return Column(
+      children: <Widget>[
+        Text(
+          'Image Widget: from Assets',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Tooltip(
+          message: "안드로이드 로고입니다.",
+          child: Image.asset('assets/images/android.png'),
+        ),
+        SizedBox(height: 20),
+        Text(
+          "Image Widget: form Network",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Tooltip(
+          message: "네트워크로부터 불러온 이미지",
+          child: Image.network(
+            "https://mblogthumb-phinf.pstatic.net/MjAyMTA4MDJfMTM3/MDAxNjI3ODY3Mjg4NzA2.LYzEC3n2-LZ9nekwfwcviDfRQRC91Eec4TO5SmYQuIkg.EtgVKqrz_PmQeiZ-hJCGf9qvFjA__prA-70BuwnqjXQg.JPEG.wenice777/3.jpeg?type=w800",
+            width: 200,
+          ),
+        ),
+        SizedBox(height: 20),
+        Text(
+          "Icon Widgets",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Icon(Icons.favorite, color: Colors.red),
+            Icon(Icons.mail, color: Colors.blue),
+            Icon(Icons.settings, color: Colors.green),
+          ],
+        ),
+      ],
     );
   }
 }
